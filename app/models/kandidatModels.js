400488a3cd3f43ff9e0ccbc3291053aacd718dbd
Satra Shufi <satraaliyu28@gmail.com>
2024-06-11 14:18:16 +0700

    Update kandidatModels.js

--- app/models/kandidatModels.js
+++ app/models/kandidatModels.js
@@ -1,4 +1,4 @@
-const mysql2 = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -19,11 +19,6 @@ exports.addKandidat = async (kandidat) => {
     await pool.query('INSERT INTO kandidat (nama_kandidat, kelas, visi, misi, profile) VALUES (?, ?, ?, ?, ?)', [nama_kandidat, kelas, visi, misi, profiles]);
 };
 
-exports.addPemilih = async () => {
-    const {nama_pemilih, kelas, role} = pemilih;
-    await pool.query('INSERT INTO pemilih (nama_pemilih, kelas, role) VALUES (?,?,?)', [nama_pemilih, kelas, role]);
-};
-
 exports.getKandidatByName = async (nama_kandidat) => {
     const [rows] = await pool.query('SELECT * FROM kandidat WHERE nama_kandidat = ?', [nama_kandidat]);
     return rows;
@@ -33,3 +28,7 @@ exports.updateKandidat = async (id_kandidat, kandidat) => {
     const {nama_kandidat, kelas, visi, misi, profiles} = kandidat;
     await pool.query('UPDATE kandidat SET nama_kandidat = ?, kelas = ?, visi = ?, misi = ?, profiles = ? WHERE id_kandidat = ?', [nama_kandidat, kelas, visi, misi, profiles, id_kandidat]);
 };
+
+exports.deleteKandidat = async (id_kandidat) => {
+    await pool.query('DELETE FROM kandidat WHERE id_kandidat = ?', [id_kandidat]);
+}
