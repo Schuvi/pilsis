c338780fd6b9014d43292214c6f1ddb5b76f2b73
Satra Shufi <satraaliyu28@gmail.com>
2024-06-12 03:38:24 +0700

    Update kandidatController.js

--- app/controllers/kandidatController.js
+++ app/controllers/kandidatController.js
@@ -31,5 +31,28 @@ exports.getEditKandidat = async (req, res) => {
 exports.updateKandidat = async (req, res) => {
     try {
         const {nama_kandidat, kelas, visi, misi, profiles} = req.body;
+        await Kandidat.updateKandidat(req.params.id, {nama_kandidat, kelas, visi, misi, profiles})
+        res.redirect('/kandidat');
+    } catch (error) {
+        res.status(500).send(error.message);
     }
-}
+};
+
+exports.searchKandidat = async (req, res) => {
+    try {
+        const nama_kandidat = req.query.nama_kandidat;
+        const kandidat = await Kandidat.getKandidatByName(nama_kandidat);
+        res.render('kandidat/search', {kandidat});
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deleteKandidat = async (req, res) => {
+    try {
+        await Kandidat.deleteKandidat(req.params.id);
+        res.redirect('/kandidat')
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
